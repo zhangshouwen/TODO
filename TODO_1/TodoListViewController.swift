51834213684d279bfa0f2a6761c8f17762b092f5
张守文 <shouwen.cn@gmail.com>
2019-01-05 22:15:16 +0800

    添加项目功能完成！

--- TODO_1/TodoListViewController.swift
+++ TODO_1/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["购买水杯","吃药","修改密码"]
+    let var itemArray = ["购买水杯","吃药","修改密码"]
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for:indexPath)
@@ -39,6 +39,31 @@ class TodoListViewController: UITableViewController {
     
     }
 
-
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        
+        
+        
+        let alert = UIAlertController(title: "添加一个新的TODO项目", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "添加项目", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "创建一个新项目..."
+            textField = alertTextField
+        }
+        
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
